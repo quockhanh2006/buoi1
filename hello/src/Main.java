1878e91048cb6b1809994c2f9f713e2244b3f794
Quốc Khánh 2006 <nguyenquockhanh05926@gmail.com>
2026-06-20 10:01:35 +0700

    update code

---
+++ hello/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 100;
+        int c = 18;
+
+        int max = a;
+
+        if (b > max) {
+            max = b;
+        }
+
+        if (c > max) {
+            max = c;
+        }
+
+        System.out.println("Số lớn nhất là: " + max);
+    }
+}
